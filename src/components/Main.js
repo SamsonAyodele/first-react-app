fcf9e06b0633c915f5668dd681edf4a10dccd3ed
Samson Ayodele <idowusamson0777@gmail.com>
2022-06-22 01:54:12 +0100

    Second commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+        <main>
+            <h1 className="main--title">fun facts about react</h1>
+             <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by facebook</li>
+                <li>Powers thousands of enterprises apps, including mobile apps</li>
+             </ul>  
+        </main>
+    )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import logo from "../logo192.png";
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} />;
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={logo} className="nav--logo" />;
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
