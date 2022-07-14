a0764eb845de110bbc587ab154a77ec5fd841e76
Samson Ayodele <idowusamson0777@gmail.com>
2022-07-14 15:24:44 +0100

    Toggler

--- src/App.js
+++ src/App.js
@@ -3,36 +3,19 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 export default function App() {
+  const [darkMode, setDarkMode] = React.useState(true)
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar 
+       darkMode={darkMode}
+       toggleDarkMode={toggleDarkMode} 
+      />
+      <Main darkMode={darkMode} />
     </div>
   )
 }
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
